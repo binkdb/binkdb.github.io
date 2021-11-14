e22536d1fa879e45d01309255c1b1bf3f629bc3a
binkdb <darren@dbnetworkdesigns.com>
2021-11-14 20:24:58 +0000

    Uploaded test-site

---
+++ scripts/main1.js
@@ -0,0 +1,6 @@
+// const myHeading = document.querySelector('h1');
+document.querySelector('h1').textContent = 'Hello world!';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
